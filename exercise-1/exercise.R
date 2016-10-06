ece7437bdba97c1f01e05304fbebfb47c9cad506
John <John>
2016-10-06 15:10:01 -0700

    added exercise 1

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -1,26 +1,25 @@
 # Exercise 1: Creating and Indexing Vectors
 
 # Create a vector `x` that has the values 10 through 20 in it (using the `:` operator)
-
+x <- c(10:20)
 
 # Create a vector `y` that has the values 21 through 30 in it (using the `seq` operator)
-
+y <- seq(21, 30)
 
 # Create a vector `z` by combining the vectors `x` and `y`
-
+z <- c(x,y)
 
 # Create a variable `w` that is equal to the 11th element in your vector `z`
-
+w <- z[11]
 
 # Create a vector `q` that is equal to the second through the 5 elements of `z`
-
+q <- c(z[2]:z[5])
 
 # Create a variable `len` that is equal to the length of your vector `q`
-
+len <- length(q)
 
 ### Bonus ###
 
 # Create a vector `odd` that holds the odd numbers from 1 to 100
-
 
 # Using the `all` function, confirm that all of the numbers in your `odd` vector are odd
